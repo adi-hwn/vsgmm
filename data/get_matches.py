7cc7eb6881fa0098043facbbf8fb9a9054f9ea12
adi-hwn <adi.hwn@gmail.com>
2018-06-13 23:53:11 +0700

    plotting

--- data/get_matches.py
+++ data/get_matches.py
@@ -1,19 +1,20 @@
-import dota2api, json, time
+import dota2api, json, time, os
 
 target_id = 20 # Vengeful Spirit
 target_mode = 22 # AP
 target_lobby = 7 # Ranked
 outfile_prefix = "sample_match_"
 
-batch_interval = 8.0
-run_interval = 120.0
+batch_interval = 6.0
+run_interval = 75.0
 
-num_passes_per_batch = 2
+num_passes_per_batch = 6
 
 api = dota2api.Initialise(open("../sensitive/steamAPIKey").read().strip())
 runno = 1
 
-last_batch_id = 3950056442
+last_batch_id = int(os.environ["LBID"])
+print(last_batch_id)
 total_parsed = 0
 passes_left = num_passes_per_batch
 recorded_matches = {last_batch_id: True} # Ensure no duplicates
@@ -23,13 +24,17 @@ while True:
     oldest_match_id = 5000000000
     oldest_match_time = 2000000000
     while oldest_match_id > last_batch_id:
-        print("run",runno,"batch",batchno,"total matches parsed:", total_parsed, "oldest_match_id:", oldest_match_id)
-        batchno += 1
         hdict = api.get_match_history(hero_id=target_id,game_mode=target_mode,start_at_match_id=oldest_match_id)
+        batchno += 1
+        print("run",runno,"batch",batchno,"total matches parsed:", total_parsed, "oldest_match_id:", oldest_match_id,hdict["results_remaining"],"/", hdict["total_results"])
+
+        if hdict["results_remaining"] == 0:
+            break
 
         for match in hdict["matches"]:
             mid = match["match_id"]
-            if mid < oldest_match_id:
+            if match["start_time"] < oldest_match_time:
+                oldest_match_time = match["start_time"]
                 oldest_match_id=mid
             #print("Testing match", mid)
             if mid in recorded_matches:
@@ -57,6 +62,9 @@ while True:
         last_batch_id = max(recorded_matches.keys())
         recorded_matches = {last_batch_id: True}
         passes_left = num_passes_per_batch
+
+    os.environ["LBID"] = str(max(recorded_matches.keys()))
+    print(os.environ["LBID"])
 
     print("run",runno,"completed, last_batch_id:", last_batch_id)
     runno += 1
